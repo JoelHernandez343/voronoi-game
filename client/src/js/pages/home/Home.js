4436f8f93d90e46f446482bc323d6a6331172a88
JoelHernandez343 <joel.programador@gmail.com>
2021-05-24 12:20:21 -0500

    Added others main sections

--- client/src/js/pages/home/Home.js
+++ client/src/js/pages/home/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
   <div className="home">
     <div className="home-content quicksand text-lg text-gray-100 h-screen md:h-auto md:backdrop-filter md:backdrop-blur-lg p-12 md:rounded-lg md:max-w-3xl md:mx-6 md:top-1/4">
       <h2 className="text-3xl font-bold">Conquista el</h2>
-      <h1 className="text-8xl font-bold">Mundo</h1>
+      <h1 className="text-8xl font-bold -m-1.5">Mundo</h1>
       <p>Bienvenido, seleccione alguna opción</p>
       <p className="mb-3"></p>
       <div className="flex space-x-3 justify-center md:justify-start">
@@ -15,9 +15,8 @@ const Home = () => (
         <MainButton icon="supervised_user_circle">Unirse a partida</MainButton>
       </div>
     </div>
-    <div className="home-bg">
-      <HomeBg />
-    </div>
+
+    <HomeBg />
   </div>
 );
 

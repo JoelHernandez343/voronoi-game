baff5b608bd5322d5f4e711098784b340b9e4d24
JoelHernandez343 <joel.programador@gmail.com>
2021-05-26 01:35:30 -0500

    Refactored navbar

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,51 +1,37 @@
 import '../css/App.css';
 import '../css/fonts.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-import { github } from './imports/images.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home/Home.js';
+import Loading from './pages/loading/Loading.js';
+import NavBar from './components/NavBar.js';
 
-const App = () => (
-  <Router>
-    <div>
-      <nav className="fixed z-10 w-full">
-        <ul className="flex space-x-24 justify-end items-center py-5 pr-24 quicksand font-semibold text-gray-200">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/rules">Reglas del juego</Link>
-          </li>
-          <li>
-            <Link to="/about">Sobre</Link>
-          </li>
-          <li>
-            <a
-              href="https://github.com/JoelHernandez343/voronoi-game"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Fork me on github"
-            >
-              <img className="w-6" src={github} alt="" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <p>This is about</p>
-        </Route>
-        <Route path="/*">
-          <p>This is 404</p>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/rules">
+            <p>This is about</p>
+          </Route>
+          <Route exact path="/about">
+            <p>This is about</p>
+          </Route>
+          <Route exact path="/loading">
+            <Loading />
+          </Route>
+          <Route path="/*">
+            <p>This is 404</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ client/src/js/components/NavBar.js
@@ -0,0 +1,34 @@
+import { Link, useLocation } from 'react-router-dom';
+import { github } from '../imports/images.js';
+
+const NavBar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="fixed z-10 w-full">
+      <ul className="flex space-x-24 justify-end items-center py-5 pr-24 quicksand font-semibold text-gray-200">
+        <li className={pathname === '/' ? 'border-b-2' : ''}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={pathname === '/rules' ? 'border-b-2' : ''}>
+          <Link to="/rules">Reglas del juego</Link>
+        </li>
+        <li className={pathname === '/about' ? 'border-b-2' : ''}>
+          <Link to="/about">Sobre</Link>
+        </li>
+        <li>
+          <a
+            href="https://github.com/JoelHernandez343/voronoi-game"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Fork me on github"
+          >
+            <img className="w-6" src={github} alt="" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
